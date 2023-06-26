126d667a4e8d2f71a378d5fc78a2d8955652329d
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-06-26 02:00:16 +0000

    roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,43 +1,11 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if roman_string is not None and type(roman_string) == str:
-        rom_dict = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
-        total = 0
-        prev = 1001
-        for c in roman_string:
-            if rom_dict[c] > prev:
-                total += rom_dict[c] - (prev * 2)
-            else:
-                total += rom_dict[c]
-            prev = rom_dict[c]
-            return total
-    return 0
-
-
-if __name__ == '__main__':
-    """ Roman to Integer test file
-    """
-    roman_to_int = __import__('12-roman_to_int').roman_to_int
-    roman_number = "X"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-    roman_number = "VII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-    roman_number = "IX"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-    roman_number = "LXXXVII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-    roman_number = "DCCVII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-    roman_number = "CM"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-    roman_number = "XCIX"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+    if not roman_string or not isinstance(roman_string, str):
+        return (0)
+    Rm = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    rm = roman_string
+    dlist = [Rm[i[0]] if Rm[i[0]] >= Rm[i[1]] else (-1*Rm[i[0]])
+             for i in zip(rm, rm[1:] + rm[-1])]
+    dec = sum(dlist)
+    return (dec)
